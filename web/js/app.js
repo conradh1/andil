335af1bb912a1a1ac0f091697b2e4daeb51c8602
conradh1 <conradh1@gmail.com>
2017-03-14 15:20:32 -1000

    Added separate navigation pages.

---
+++ web/js/app.js
@@ -0,0 +1,26 @@
+/*
+ * This is a JS app for page routing and templating.
+ *
+ * */
+
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'home.html'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            url: '/about',
+            templateUrl: 'about.html'
+        });
+});
+
