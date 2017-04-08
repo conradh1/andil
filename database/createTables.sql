e8db0a03cee6c66108d876fbfa3aaafae8cebf4a
conradh1 <conradh1@gmail.com>
2017-04-07 14:07:23 -1000

    Modified create tables script to work.

--- database/createTables.sql
+++ database/createTables.sql
@@ -1,30 +1,29 @@
 
 /* Providers are the users who provide experiences */
 CREATE TABLE providers (
-pk_id INT UNSIGNED AUTO_INCREMENT,
-login VARCHAR(30) NOT NULL,
-pass VARCHAR(256) NOT NULL,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-address VARCHAR(30) NOT NULL,
-city VARCHAR(30) NOT NULL,
-island VARCHAR(30) NOT NULL,
-state VARCHAR(30) NOT NULL,
-zip VARCHAR(30) NOT NULL,
-email VARCHAR(50) NOT NULL,
-phone VARCHAR(30),
-insta_login VARCHAR(30),
-profiile_image_path_1 VARCHAR(30) NOT NULL,
-insta_pass VARCHAR(30),
-whatsap VARCHAR(30),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-active ENUM('yes','no'),
-PRIMARY KEY (pk_id)
-)
+  pk_id INT PRIMARY KEY AUTO_INCREMENT,
+  login VARCHAR(30) NOT NULL,
+  pass VARCHAR(256) NOT NULL,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  address VARCHAR(30) NOT NULL,
+  city VARCHAR(30) NOT NULL,
+  island VARCHAR(30) NOT NULL,
+  state VARCHAR(30) NOT NULL,
+  zip VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  phone VARCHAR(30),
+  insta_login VARCHAR(30),
+  profiile_image_path_1 VARCHAR(30) NOT NULL,
+  insta_pass VARCHAR(30),
+  whatsap VARCHAR(30),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  active ENUM('yes','no')
+) ENGINE=INNODB;
 
 /* Seekers are users who wish to engage in an experience */
 CREATE TABLE seekers (
-  pk_id INT UNSIGNED AUTO_INCREMENT,
+  pk_id INT PRIMARY KEY AUTO_INCREMENT,
   login VARCHAR(30) NOT NULL,
   pass VARCHAR(256) NOT NULL,
   firstname VARCHAR(30),
@@ -41,13 +40,18 @@ CREATE TABLE seekers (
   gender VARCHAR(30),
   reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   whatsap VARCHAR(30),
-  active ENUM('yes','no'),
-  PRIMARY KEY (pk_id)
-);
+  active ENUM('yes','no')
+) ENGINE=INNODB;
+
+CREATE TABLE experience_types (
+  pk_id SMALLINT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(256)
+) ENGINE=INNODB;
+
 
 /* Contains details of every experience */
 CREATE TABLE experiences (
-  pk_id INT UNSIGNED AUTO_INCREMENT,
+  pk_id INT PRIMARY KEY AUTO_INCREMENT,
   fk_provider_id INT NOT NULL,
   fk_type_id SMALLINT NOT NULL,
   slogan VARCHAR(64) NOT NULL,
@@ -66,33 +70,28 @@ CREATE TABLE experiences (
   video_path_1 VARCHAR(30),
   video_path_2 VARCHAR(30),
   reg_date TIMESTAMP,
-  PRIMARY KEY (pk_id),
-  FOREIGN KEY (fk_provider_id) REFERENCES providers(pk_id),
-  FOREIGN KEY (fk_type_id) REFERENCES experience_types(pk_id)
+  FOREIGN KEY(fk_provider_id) REFERENCES providers(pk_id)
+   ON DELETE CASCADE,
 
-);
+  FOREIGN KEY (fk_type_id) REFERENCES experience_types(pk_id)
+   ON DELETE CASCADE
+) ENGINE=INNODB;
 
 /* Seekers are users who wish to engage in an experience */
 CREATE TABLE transactions (
-  pk_id INT UNSIGNED AUTO_INCREMENT,
+  pk_id INT PRIMARY KEY AUTO_INCREMENT,
   reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  fk_provider_id INT NOT NULL,
-  fk_seeker_id INT NOT NULL,
   fk_experience_id INT NOT NULL,
-  rating ENUM(1,2,3,4,5),
+  fk_seeker_id INT NOT NULL,
+  rating ENUM('1','2','3','4','5'),
   comments VARCHAR(200),
-  PRIMARY KEY (pk_id),
-  FOREIGN KEY (fk_seeker_id) REFERENCES seekers(pk_id),
-  FOREIGN KEY (fk_provider_id) REFERENCES providers(pk_id),
-  FOREIGN KEY (fk_experience_id) REFERENCES experiences(pk_id),
-);
+  FOREIGN KEY(fk_experience_id) REFERENCES experiences(pk_id)
+   ON DELETE CASCADE,
+  FOREIGN KEY(fk_seeker_id) REFERENCES seekers(pk_id)
+   ON DELETE CASCADE
+) ENGINE=INNODB;
 
 
-/
-CREATE TABLE experience_types (
-  pk_id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(256)
 
-)
 
 
